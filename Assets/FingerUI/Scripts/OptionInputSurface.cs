d4e06f2b408914cf5d93e83073096d9f3e9003fd
WSID <jongsome@gmail.com>
2016-07-05 20:07:17 +0900

    OptionInputSurface: Remove default parameter.

--- Assets/FingerUI/Scripts/OptionInputSurface.cs
+++ Assets/FingerUI/Scripts/OptionInputSurface.cs
@@ -9,7 +9,7 @@ public struct OptionInputSurface
 	public Vector3 normal;
 	public Vector3 up;
 
-	public OptionInputSurface (Vector3 position, Vector3 normal, Vector3 up = Vector3.up) {
+	public OptionInputSurface (Vector3 position, Vector3 normal, Vector3 up) {
 		this.position = position;
 		this.normal = normal;
 		this.up = up;
