767539e5a3caf9d5b81e319b1d7c8a91107d57c9
WSID <jongsome@gmail.com>
2016-07-05 20:08:36 +0900

    OptionBehaviour: Added
    This will manage option items.
    
    Tasks for this class is,
     - Manage option items.
     - Show option panels or option scenes.
     - Store and load option values from storage.

---
+++ Assets/FingerUI/Scripts/OptionBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OptionBehaviour : MonoBehaviour {
+	public static Dictionary<string, Object> optionResults = null;
+
+	public const string KEY_OPTION_INPUT_SURFACE = "input-surface";
+
+	// Use this for initialization
+	void Start () {
+		if (optionResults == null) {
+			optionResults = new Dictionary <string, Object> ();
+			Debug.Log ("Option cleared!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void showOptionInputSurface () {
+		optionResults.Remove (KEY_OPTION_INPUT_SURFACE);
+
+		SceneManager.LoadScene ("OptionInputSurface");
+		//SceneManager.SetActiveScene (SceneManager.GetSceneByPath ("Assets/FingerUI/OptionInputSurface.unity"));
+	}
+}
