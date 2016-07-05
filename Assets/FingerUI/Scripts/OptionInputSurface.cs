ed63251f3577b915beb5f1146ccfe253f272f74a
WSID <jongsome@gmail.com>
2016-07-05 19:41:46 +0900

    OptionInputSurface: Added OptionInputSurface.
    This will carry information for Input Surface, so that this information
    can be transferred between FingerUI2 scene and OptionInputSurface scene.

---
+++ Assets/FingerUI/Scripts/OptionInputSurface.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+
+public struct OptionInputSurface
+{
+	public static OptionInputSurface optionResult = null;
+
+	public Vector3 position;
+	public Vector3 normal;
+	public Vector3 up;
+
+	public OptionInputSurface (Vector3 position, Vector3 normal, Vector3 up = Vector3.up) {
+		this.position = position;
+		this.normal = normal;
+		this.up = up;
+	}
+
+	public void ApplyTo (Transform transform) {
+		transform.position = position;
+		transform.rotation = Quaternion.LookRotation (normal, up);
+	}
+
+	public void ApplyTo (GameObject gameObject) {
+		ApplyTo (gameObject.transform);
+	}
+}
+
