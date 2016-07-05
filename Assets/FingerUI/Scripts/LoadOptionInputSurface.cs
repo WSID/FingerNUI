7a9bd9a169899d1b512300da5c39863c2bbd9e12
WSID <jongsome@gmail.com>
2016-07-05 21:06:05 +0900

    LoadOptionInputSurfaceBehaviour: Added class.
    This class will load OptionInputSurface and set it for the camera.

---
+++ Assets/FingerUI/Scripts/LoadOptionInputSurface.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOptionInputSurface : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int optionInputSurfaceSet = PlayerPrefs.GetInt ("option-input-surface-set");
+
+		if (optionInputSurfaceSet == 1) {
+			OptionInputSurface option = OptionInputSurface.load_pref ("option-input-surface");
+
+			option.ApplyTo (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
