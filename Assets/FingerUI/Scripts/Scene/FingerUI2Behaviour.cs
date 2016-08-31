b4e35f07f4e90d8d9cb5fa578ab4723243874bd9
WSID <jongsome@gmail.com>
2016-08-31 16:02:07 +0900

    FingerUI2Behaviour: Crunch some scene specific functions into one class.

---
+++ Assets/FingerUI/Scripts/Scene/FingerUI2Behaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+using System;
+
+public class FingerUI2Behaviour : MonoBehaviour
+{
+	public HandController handController;
+
+	void Start () {
+		int optionInputSurfaceSet = PlayerPrefs.GetInt ("option-input-surface-set");
+
+		if (optionInputSurfaceSet == 1) {
+			OptionInputSurface option = OptionInputSurface.load_pref ("option-input-surface");
+
+			option.ApplyTo (gameObject);
+		}
+	}
+
+	public void Quit () {
+		Application.Quit ();
+	}
+
+	public void ShowScene (string sceneName) {
+		SceneManager.LoadScene (sceneName);
+	}
+}
+
